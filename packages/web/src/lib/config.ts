d55e6c03a34466540f743b3ad38918e04cc90de2
snoopy1412 <snoopy1412.su@gmail.com>
2025-03-25 23:44:06 +0800

    新增 degov2.yml 配置文件并更新 getDaoConfigServer 函数以支持新配置路径

--- packages/web/src/lib/config.ts
+++ packages/web/src/lib/config.ts
@@ -13,16 +13,25 @@ export const getDaoConfigServer = (): Config => {
   try {
     const configPath = path.join(process.cwd(), "public", "degov.yml");
 
+    const configPath2 = path.join(process.cwd(), "public", "degov2.yml");
+
     console.log("configPath", configPath);
     console.log("process.cwd()", process.cwd());
 
     let yamlText: string | undefined;
-
+    let yamlText2: string | undefined;
     try {
       yamlText = fs.readFileSync(configPath, "utf8");
       console.log(`[Config] Loaded from ${configPath}`);
     } catch {
       console.log("[Config] Using default config");
+      return defaultConfig as Config;
+    }
+    try {
+      yamlText2 = fs.readFileSync(configPath2, "utf8");
+      console.log(`[Config2] Loaded from ${configPath2}`);
+    } catch {
+      console.log("[Config2] Using default config");
       return defaultConfig as Config;
     }
 
